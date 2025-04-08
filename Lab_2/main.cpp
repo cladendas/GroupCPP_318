3d2a5669fcaf0978435bd9d9c26518b5f5225d00
cladendas <rvdagi05@yandex.ru>
2025-04-08 14:04:42 +0300

    Update main.cpp

--- Lab_2/main.cpp
+++ Lab_2/main.cpp
@@ -29,7 +29,7 @@ double get_index_weight(const int height, const double weight) {
 std::string index_weight(const int height, const double weight) {
     const double index{get_index_weight(height, weight)}; //получение индекса
 
-    std::cout << "Индекс: " << index << std::endl;
+    std::cout << "Индекс: " << index << '\n';
 
     if (index <= 16) {
         return "Выраженный дефицит массы";
@@ -89,9 +89,9 @@ std::string index_weight_age(const int height, const double weight, const bool s
 
 void task_1() {
     //Без проверок данных введёных пользователем
-    std::cout << "Задание 1" << std::endl;
-    std::cout << "Калькулятор для расчета идеального веса" << std::endl;
-    std::cout << "Калькулятор для расчета идеального веса" << std::endl;
+    std::cout << "Задание 1" << '\n';
+    std::cout << "Калькулятор для расчета идеального веса" << '\n';
+    std::cout << "Калькулятор для расчета идеального веса" << '\n';
 
     std::cout << "Введите возраст: ";
     int tmp_age;
@@ -113,10 +113,10 @@ void task_1() {
     std::cin >> tmp_sex;
     const int sex{tmp_sex};
 
-    std::cout << "Подсчет идеального веса" << std::endl;
-    std::cout << "a - по формуле Брока" << std::endl;
-    std::cout << "b - по индексу массы тела" << std::endl;
-    std::cout << "с - по индексу массы тела с учетом возраста" << std::endl;
+    std::cout << "Подсчет идеального веса" << '\n';
+    std::cout << "a - по формуле Брока" << '\n';
+    std::cout << "b - по индексу массы тела" << '\n';
+    std::cout << "с - по индексу массы тела с учетом возраста" << '\n';
     char tmp_calculation;
     std::cin >> tmp_calculation;
     const char calculation{tmp_calculation};
@@ -124,15 +124,15 @@ void task_1() {
     if (calculation == 'a') {
         std::cout << "Оптимальный вес должен быть в районе: " << brock(height, sex);
     } else if (calculation == 'b') {
-        std::cout << index_weight(height, weight) << std::endl;
+        std::cout << index_weight(height, weight) << '\n';
     } else if (calculation == 'c') {
-        std::cout << index_weight_age(height, weight, sex, age) << std::endl;
+        std::cout << index_weight_age(height, weight, sex, age) << '\n';
     }
 }
 
 //отображение типа и занимаемого им места в байтах
 void show_sizeof(const std::string type, const int size) {
-    std::cout << type << ": " << size << " bytes" << std::endl;
+    std::cout << type << ": " << size << " bytes" << '\n';
 }
 
 
@@ -143,18 +143,18 @@ void show_sizeof(const std::string type, const int size) {
 //генерация блоков кода для вывода мин и макс значений тип type
 #define TYPE_MIN_MAX(type) { \
     const unsigned short bits = 16; \
-    std::cout << "Binary" << std::endl; \
-    std::cout << "- min: " << std::bitset<bits>{std::numeric_limits<type>::min()} << std::endl; \
-    std::cout << "- max: " << std::bitset<bits>{std::numeric_limits<type>::max()} << std::endl; \
-    std::cout << "Octal" << std::endl; \
-    std::cout << "- min: " << std::oct << std::numeric_limits<type>::min() << std::endl; \
-    std::cout << "- max: " << std::oct << std::numeric_limits<type>::max() << std::endl; \
-    std::cout << "Decimal" << std::endl; \
-    std::cout << "- min: " << std::dec << std::numeric_limits<type>::min() << std::endl; \
-    std::cout << "- max: " << std::dec << std::numeric_limits<type>::max() << std::endl; \
-    std::cout << "hexadecimal" << std::endl; \
-    std::cout << "- min: " << std::hex << std::numeric_limits<type>::min() << std::endl; \
-    std::cout << "- max: " << std::hex << std::numeric_limits<type>::max() << std::endl; \
+    std::cout << "Binary" << '\n'; \
+    std::cout << "- min: " << std::bitset<bits>{std::numeric_limits<type>::min()} << '\n'; \
+    std::cout << "- max: " << std::bitset<bits>{std::numeric_limits<type>::max()} << '\n'; \
+    std::cout << "Octal" << '\n'; \
+    std::cout << "- min: " << std::oct << std::numeric_limits<type>::min() << '\n'; \
+    std::cout << "- max: " << std::oct << std::numeric_limits<type>::max() << '\n'; \
+    std::cout << "Decimal" << '\n'; \
+    std::cout << "- min: " << std::dec << std::numeric_limits<type>::min() << '\n'; \
+    std::cout << "- max: " << std::dec << std::numeric_limits<type>::max() << '\n'; \
+    std::cout << "hexadecimal" << '\n'; \
+    std::cout << "- min: " << std::hex << std::numeric_limits<type>::min() << '\n'; \
+    std::cout << "- max: " << std::hex << std::numeric_limits<type>::max() << '\n'; \
 }
 
 void task_2() {
@@ -165,7 +165,7 @@ void task_2() {
     TYPE_SIZE(char)
     TYPE_SIZE(bool)
 
-    std::cout << std::endl;
+    std::cout << '\n';
 
     TYPE_SIZE(unsigned int)
     TYPE_SIZE(signed int)
@@ -176,29 +176,29 @@ void task_2() {
     TYPE_SIZE(unsigned short)
     TYPE_SIZE(unsigned long long)
 
-    std::cout << std::endl;
+    std::cout << '\n';
 
-    std::cout << "unsigned short --------" << std::endl;
+    std::cout << "unsigned short --------" << '\n';
     TYPE_MIN_MAX(unsigned short)
 
-    std::cout << std::endl;
+    std::cout << '\n';
 
-    std::cout << "unsigned int --------" << std::endl;
+    std::cout << "unsigned int --------" << '\n';
     TYPE_MIN_MAX(unsigned int)
 
-    std::cout << std::endl;
+    std::cout << '\n';
 
-    std::cout << "bool --------" << std::endl;
+    std::cout << "bool --------" << '\n';
     TYPE_MIN_MAX(bool)
 
-    std::cout << std::endl;
+    std::cout << '\n';
 
-    std::cout << "u_int32_t --------" << std::endl;
+    std::cout << "u_int32_t --------" << '\n';
     TYPE_MIN_MAX(u_int32_t)
 
-    std::cout << std::endl;
+    std::cout << '\n';
 
-    std::cout << "unsigned long --------" << std::endl;
+    std::cout << "unsigned long --------" << '\n';
     TYPE_MIN_MAX(unsigned long)
 
 }
@@ -221,21 +221,21 @@ double get_amount(const int quantity) {
 
 void task_3() {
     const size_t quantity = 10; //кол-во запросов
-    std::cout << "Сейчас у вас запросят 10 раз ввод числа с точкой" << std::endl;
+    std::cout << "Сейчас у вас запросят 10 раз ввод числа с точкой" << '\n';
     double amount = get_amount(quantity);
 
-    std::cout << "Среднее арифметическое: " << (amount / quantity) << std::endl;
+    std::cout << "Среднее арифметическое: " << (amount / quantity) << '\n';
 }
 
 void task_4() {
-    std::cout << "34.50 = " << 3.450e1 << std::endl;
-    std::cout << "0.004000 = " << 4e-3 << std::endl;
-    std::cout << "123.005 = " << 1.23005e2 << std::endl;
-    std::cout << "146000 = " << 1.46e5 << std::endl;
+    std::cout << "34.50 = " << 3.450e1 << '\n';
+    std::cout << "0.004000 = " << 4e-3 << '\n';
+    std::cout << "123.005 = " << 1.23005e2 << '\n';
+    std::cout << "146000 = " << 1.46e5 << '\n';
 }
 
 //генерация кода для проверки истинности выражений
-#define CHECK_EXPRESSION(expr) std::cout << #expr" = " << std::boolalpha << expr << std::endl;
+#define CHECK_EXPRESSION(expr) std::cout << #expr" = " << std::boolalpha << expr << '\n';
 
 enum class Nums {
     ONE  = 1,
@@ -264,14 +264,14 @@ void task_5() {
         const bool f = true;
 
         std::cout << std::boolalpha;
-        std::cout << ( ((a && b) || ( !c) && (d || f)) ) << std::endl;
-        std::cout << ( ((a && b) || (c && d) || (!e)) ) << std::endl;
-        std::cout << ( ((a || b) && (c || d) && (e || f)) ) << std::endl;
+        std::cout << ( ((a && b) || ( !c) && (d || f)) ) << '\n';
+        std::cout << ( ((a && b) || (c && d) || (!e)) ) << '\n';
+        std::cout << ( ((a || b) && (c || d) && (e || f)) ) << '\n';
 
-        std::cout << ( ((Nums::ONE > Nums::TWO) && (Nums::THREE < Nums::FOURE) && (Nums::FIVE != Nums::SIX)) ) << std::endl;
+        std::cout << ( ((Nums::ONE > Nums::TWO) && (Nums::THREE < Nums::FOURE) && (Nums::FIVE != Nums::SIX)) ) << '\n';
 
-        std::cout << "Закон де Моргана !(a && B) == !a || !b" << " --- " << ( !(a && b) == !a || !b ) << std::endl;
-        std::cout << "Закон де Моргана !(a || B) == !a && !b" << " --- " << ( !(a || b) == !a && !b ) << std::endl;
+        std::cout << "Закон де Моргана !(a && B) == !a || !b" << " --- " << ( !(a && b) == !a || !b ) << '\n';
+        std::cout << "Закон де Моргана !(a || B) == !a && !b" << " --- " << ( !(a || b) == !a && !b ) << '\n';
     }
 
     {
@@ -283,14 +283,14 @@ void task_5() {
 
         std::cin >> x >> y >> z;
 
-        std::cout << "1) " << "x = 3 + 4 + 5 = " << x + 3 + 4 + 5 << std::endl;
-        std::cout << "2) " << "x = y = x = " << (x == y == z) << std::endl;
+        std::cout << "1) " << "x = 3 + 4 + 5 = " << x + 3 + 4 + 5 << '\n';
+        std::cout << "2) " << "x = y = x = " << (x == y == z) << '\n';
         z *= ++y + 5;
-        std::cout << "3) " << "z *= ++y + 5 = " << z << std::endl;
+        std::cout << "3) " << "z *= ++y + 5 = " << z << '\n';
 
         bool logicValue;
         logicValue = x || y && z || v;
-        std::cout << "logicValue = x || y && z || v = " << logicValue << std::endl;
+        std::cout << "logicValue = x || y && z || v = " << logicValue << '\n';
     }
 
     {
@@ -303,10 +303,10 @@ void task_5() {
 
         num <<= shift;
 
-        std::cout << num << std::endl;
-        std::cout << std::bitset<8>(num) << std::endl;
-        std::cout << std::hex << num << std::endl;
-        std::cout << std::oct << num << std::endl;
+        std::cout << num << '\n';
+        std::cout << std::bitset<8>(num) << '\n';
+        std::cout << std::hex << num << '\n';
+        std::cout << std::oct << num << '\n';
 
     }
 
